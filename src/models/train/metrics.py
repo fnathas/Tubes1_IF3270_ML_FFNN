7c409c2cb9aae95afbf1fc8097dbba2fc2d9fe87
fnathas <farrel.saskoro@gmail.com>
2025-03-28 20:31:17 +0700

    refactor: remove unused file

--- src/models/train/metrics.py
+++
@@ -1,4 +0,0 @@
-class Metric:
-    def calculate(self, y_true, y_pred):
-        """Menghitung metrik evaluasi."""
-        raise NotImplementedError("Subclasses harus mengimplementasikan calculate.")
